04f8f74b001175f1137252d4c9cf9df50302ffea
Slava Shumakov <shum.slav@yandex.ru>
2021-05-30 05:36:06 +0500

    add all images

--- app/src/main/java/com/shumslav/cardgamefortest/Activity/GameActivity.kt
+++ app/src/main/java/com/shumslav/cardgamefortest/Activity/GameActivity.kt
@@ -6,6 +6,7 @@ import android.graphics.drawable.LevelListDrawable
 import android.graphics.drawable.TransitionDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.util.Size
 import android.view.View
 import android.view.ViewGroup
@@ -23,33 +24,31 @@ class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
-        dificult = Pair(5, 5)
-        linearLayout = findViewById(R.id.game_layout)
+        dificult = Pair(5,5)
     }
 
 
     override fun onResume() {
         super.onResume()
         val cardBoard = findViewById<TableLayout>(R.id.cardBoard)
-        size = Size(linearLayout.layoutParams.width, linearLayout.layoutParams.height)
-        val widthCard = size.width/dificult.first
-        val heightCard = size.height/dificult.second
         if(!isCreated) {
             for (i in 0..dificult.first - 1) {
                 val row = TableRow(this)
                 row.layoutParams = ViewGroup.LayoutParams(
-                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
                     ViewGroup.LayoutParams.WRAP_CONTENT
                 )
                 for (j in 0..dificult.second - 1) {
-                    val a = ResourcesCompat.getDrawable(resources, R.drawable.card1, theme)
-                    val b = ResourcesCompat.getDrawable(resources, R.drawable.airplane, theme)
-                    val card1 = TransitionDrawable(arrayOf(a, b))
-                    val card = ImageView(this)
-                    card.setImageDrawable(card1)
-                    val trans = card.drawable as TransitionDrawable
-                    card.setOnClickListener { trans.startTransition(1000) }
-                    row.addView(card)
+                    val card = ResourcesCompat.getDrawable(resources, R.drawable.card, theme)
+                    val image = ResourcesCompat.getDrawable(resources, R.drawable.airplane, theme)
+                    val cardWithImage = TransitionDrawable(arrayOf(card, image))
+                    val cardWithImageView = ImageView(this)
+                    cardWithImageView.setImageDrawable(cardWithImage)
+//                    val params = LinearLayout.LayoutParams(10,10)
+//                    cardWithImageView.layoutParams = params
+                    val trans = cardWithImageView.drawable as TransitionDrawable
+                    cardWithImageView.setOnClickListener { trans.resetTransition() }
+                    row.addView(cardWithImageView)
                 }
                 cardBoard.addView(row, i)
             }
